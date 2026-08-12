aec7eaee4fd7a4db0490074416f9292d29949c4d
Connor Kormos <connorkormos97@gmail.com>
2026-08-12 16:26:08 -0700

    updated base url on client side for api requests

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = import.meta.env.VITE_API_URL;
-export const BASE_URL = "http://free-resume-builder.railway.internal";
+export const BASE_URL = "https://resume-builder-api.up.railway.app";

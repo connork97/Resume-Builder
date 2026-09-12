287f80d4833a37069ee1f44c7782dcc5d9b398bf
Connor Kormos <connorkormos97@gmail.com>
2026-09-12 14:54:04 -0700

    same change applied for lists

--- client/src/features/ResumeEditor/TextFormatting/Lists.jsx
+++ client/src/features/ResumeEditor/TextFormatting/Lists.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { toggleList } from '../../../helpers/blocks';
+import { isBlockActive, toggleList } from '../../../helpers/blocks';
 
 import styles from './TextFormatting.module.css';
 import { MdFormatListBulleted, MdFormatListNumbered } from 'react-icons/md';
@@ -9,14 +9,20 @@ const Lists = ({ editor }) => {
   return (
     <div className={styles.toolbarFlexWrapper}>
       <button data-toolbar-label="Bulleted List"
-        className='buttonMain'
+        aria-label="Bulleted List"
+        aria-pressed={!!editor?.selection && isBlockActive(editor, "unordered-list")}
+        className={`buttonMain ${styles.listButton}`}
+        onMouseDown={(event) => event.preventDefault()}
         onClick={() => editor && toggleList(editor, "unordered-list")}
       >
         <MdFormatListBulleted style={{position: 'relative', top: '0.1em'}} />
         </button>
 
       <button data-toolbar-label="Numbered List"
-        className='buttonMain'
+        aria-label="Numbered List"
+        aria-pressed={!!editor?.selection && isBlockActive(editor, "ordered-list")}
+        className={`buttonMain ${styles.listButton}`}
+        onMouseDown={(event) => event.preventDefault()}
         onClick={() => editor && toggleList(editor, "ordered-list")}
       >
         <MdFormatListNumbered style={{position: 'relative', top: '0.1em'}} />
